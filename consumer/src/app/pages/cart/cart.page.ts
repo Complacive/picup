8765f6a6d33ff0a2960ab9d76a428e98dd1c63b4
KingBlacc <bobbycarelse@gmail.com>
2019-07-30 18:43:56 +0200

    I added my binding and mock data, just have to do the calculations now

--- consumer/src/app/pages/cart/cart.page.ts
+++ consumer/src/app/pages/cart/cart.page.ts
@@ -1,7 +1,6 @@
 import { CartService } from './../../shared/services/cart.service';
 import { Cart } from './../../shared/models/cart';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.page.html',
@@ -12,7 +11,7 @@ export class CartPage implements OnInit {
 
   public resultCart: any;
   resultProduct: any;
-  
+ 
   constructor(private cartService: CartService) { }
 
   ngOnInit() {
@@ -20,9 +19,30 @@ export class CartPage implements OnInit {
   }
 
   displayCart(){
-    return this.cartService.getCart().forEach( data => {
-      this.resultCart = data;
-    });
+    var cartJSON = [{
+      "item_id": 1,
+      "item_url": "https://cdn0.iconfinder.com/data/icons/expenses-vs-income/30/__gas_fuel_station_car-256.png",
+      "item_name": "Small Gas",
+      "total_price": 95.23,
+      "quantity": 2
+  },
+  {
+      "item_id": 2,
+      "item_url": "https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-1/135/_Canister_Egg-256.png",
+      "item_name": "Medium Gas",
+      "total_price": 147.82,
+      "quantity": 3
+  },
+  {
+      "item_id": 3,
+      "item_url": "https://cdn3.iconfinder.com/data/icons/eco-flat-2/512/gas_oil_station_petrol-256.png",
+      "item_name": "Large Gas",
+      "total_price": 289.39,
+      "quantity": 5
+  }
+];
+
+    return this.resultCart = cartJSON;
   }
 
   getSubtotal(){
